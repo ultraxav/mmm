f44132f4ef727909e483a87a19b7466016cf3b7c
Javier del Villar <ultraxav@msn.com>
2024-04-20 18:42:12 -0300

    data_engineering pipeline

---
+++ src/mmm/pipelines/data_engineering/nodes.py
@@ -0,0 +1,28 @@
+"""
+This is a boilerplate pipeline 'data_engineering'
+generated using Kedro 0.18.14
+"""
+
+import pandas as pd
+
+
+def data_cleaning(data: pd.DataFrame) -> pd.DataFrame:
+
+    data = data.rename(columns={"DATE": "date_week"})
+
+    data = pd.concat(
+        [data, pd.get_dummies(data["events"], dtype=int).iloc[:, :-1]], axis=1
+    )
+
+    data = data.drop(columns=["events"])
+
+    data["day_of_year"] = pd.to_datetime(data["date_week"]).dt.dayofyear
+
+    return data
+
+
+def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
+
+    data["t"] = range(data.shape[0])
+
+    return data
---
+++ src/mmm/pipelines/data_engineering/pipeline.py
@@ -0,0 +1,27 @@
+"""
+This is a boilerplate pipeline 'data_engineering'
+generated using Kedro 0.18.14
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import data_cleaning, feature_engineering
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                data_cleaning,
+                inputs="raw_data",
+                outputs="primary_data",
+                name="data_cleaning",
+            ),
+            node(
+                feature_engineering,
+                inputs="primary_data",
+                outputs="feature_data",
+                name="feature_engineering",
+            ),
+        ]
+    )
